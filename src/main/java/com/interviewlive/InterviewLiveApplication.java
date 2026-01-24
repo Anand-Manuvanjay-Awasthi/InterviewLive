c17440e7f2af2015ed443c6d25a6b86fb88c3f24
Anand <anand.2327cse1095@kiet.edu>
2026-01-24 22:41:46 +0530

    Initialising repo

---
+++ src/main/java/com/interviewlive/InterviewLiveApplication.java
@@ -0,0 +1,13 @@
+package com.interviewlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interviewlive/InterviewLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
